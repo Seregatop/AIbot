b795338c14e8ede33a99044661467057af3c50a7
Sergulyok <STIFT1997@mail.ru>
2024-09-24 23:24:20 +0300

    Создал бот токен

---
+++ run.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+
+from app.user import user
+from app.admin import admin
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_routers(user, admin)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Stopped')
