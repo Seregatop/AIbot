af92a702c12c63e0f0699d9d3a4d5af6df5210b8
Sergulyok <STIFT1997@mail.ru>
2024-09-30 17:05:22 +0300

    Добавление генерации фотографий

---
+++ app/states.py
@@ -0,0 +1,11 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class Chat(StatesGroup):
+    text = State()
+    wait = State()
+
+
+class Image(StatesGroup):
+    text = State()
+    wait = State()
--- app/user.py
+++ app/user.py
@@ -2,25 +2,15 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
+from app.states import Chat, Image
 
 import app.keyboards as kb
-from app.generators import gpt_text
+from app.generators import gpt_text, gpt_image
 from app.database.requests import set_user, get_user, calculate
 
 from decimal import Decimal
 
 user = Router()
-
-
-class Chat(StatesGroup):
-    text = State()
-    wait = State()
-
-
-class Image(StatesGroup):
-    text = State()
-    wait = State()
 
 
 @user.message(F.text == 'Отмена')
@@ -32,7 +22,7 @@ async def cmd_start(message: Message, state: FSMContext):
     await state.clear()
 
 
-@user.message(F.text == 'Генерация картинок')
+@user.message(F.text == 'Чат')
 async def chatting(message: Message, state: FSMContext):
     user_db = await get_user(message.from_user.id)
     if Decimal(user_db.balance) > 0:
@@ -42,7 +32,7 @@ async def chatting(message: Message, state: FSMContext):
         await message.answer('Недостаточно средств на балансе')
 
 
-@user.message()
+@user.message(Chat.text)
 async def chat_response(message: Message, state: FSMContext):
     user_db = await get_user(message.from_user.id)
     if Decimal(user_db.balance) > 0:
@@ -55,9 +45,37 @@ async def chat_response(message: Message, state: FSMContext):
         await message.answer('Недостаточно средств на балансе')
 
 
+@user.message(Image.wait)
 @user.message(Chat.wait)
 async def stop(massage: Message):
     await massage.answer('Не так быстро, дай мне придумать ответ!')
 
 # ________________________________________________________________________
+
+
+@user.message(F.text == 'Генерация картинок')
+async def chatting(message: Message, state: FSMContext):
+    user_db = await get_user(message.from_user.id)
+    if Decimal(user_db.balance) > 0:
+        await state.set_state(Image.text)
+        await message.answer('Введите запрос', reply_markup=kb.cancel)
+    else:
+        await message.answer('Недостаточно средств на балансе')
+
+
+@user.message(Image.text)
+async def chat_response(message: Message, state: FSMContext):
+    user_db = await get_user(message.from_user.id)
+    if Decimal(user_db.balance) > 0:
+        await state.set_state(Image.wait)
+        response = await gpt_image(message.text, model='dall-e-3')
+        await calculate(message.from_user.id, response['usage'], 'dall-e-3')
+        try:
+            await message.answer_photo(photo=response['response'])
+        except Exception as e:
+            print(e)
+            await message.answer(response['response'])
+        await state.set_state(Image.text)
+    else:
+        await message.answer('Недостаточно средств на балансе')
 
